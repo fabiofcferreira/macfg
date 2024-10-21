ebfe9be28fd20f1e448e6ece54760b91bd4e9ecf
Fábio Ferreira <ffcfpten@gmail.com>
2024-10-21 18:23:15 +0100

    feat: 🎸 pre run script to install oh my zsh

---
+++ pre-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "✅ Successfully installed Oh My Zsh"
