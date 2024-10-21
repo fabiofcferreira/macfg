9fa4778e07bd93ec24b9f91cc36e08b46a3307d3
Fábio Ferreira <ffcfpten@gmail.com>
2024-10-21 17:25:34 +0100

    feat: 🎸 install homebrew, nvm, node & git

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+echo "Creating Hyper.js configuration..."
 cp ./assets/hyper/.hyper.js ~/.hyper.js
+echo "✅ Successfully moved .hyper.js."
+
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "✅ Installed homebrew"
+
+echo "Installing nvm, node, git"
+brew install nvm
+nvm install lts
+brew install git
+
